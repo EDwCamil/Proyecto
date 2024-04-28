6aca95c76b581b563c4522ed4f4a320c47e241f9
EdwCami <ecmunoz@ucompensar.edu.co>
2024-04-28 15:50:05 -0500

    formularios

--- ecommerceAngular/src/app/vistas/login/login.component.ts
+++ ecommerceAngular/src/app/vistas/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule,FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class AppLogin implements OnInit {
+
+
+  get email(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  get password(){
+    return this.formUser.get('password') as FormControl;
+  }
+
+  formUser = new FormGroup ({
+    'email' : new FormControl ('',[Validators.required, Validators.email]),
+    'password' : new FormControl('', Validators.required)
+
+  });
+ 
+  
+
   constructor(){
 
   }
@@ -17,3 +38,4 @@ export class AppLogin implements OnInit {
     console.log('entre a mi login')
   }
 }
+
