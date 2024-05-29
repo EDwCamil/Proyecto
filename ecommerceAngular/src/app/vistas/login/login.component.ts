e2a216fd47f017b4ba8c4b69d830b3c81b0a3d0e
EdwCami <ecmunoz@ucompensar.edu.co>
2024-05-28 21:45:59 -0500

    cambios en login

--- ecommerceAngular/src/app/vistas/login/login.component.ts
+++ ecommerceAngular/src/app/vistas/login/login.component.ts
@@ -1,41 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule,FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AppHeader } from '../../components/header/header.component';
+import { AppFooter } from '../../components/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, AppHeader, AppFooter]
 })
 export class AppLogin implements OnInit {
-
-
-  get email(){
-    return this.formUser.get('email') as FormControl;
-  }
-
-  get password(){
-    return this.formUser.get('password') as FormControl;
-  }
-
-  formUser = new FormGroup ({
-    'email' : new FormControl ('',[Validators.required, Validators.email]),
-    'password' : new FormControl('', Validators.required)
-
-  });
- 
-  
-
-  constructor(){
+  loginForm = this.formBuilder.group({
+    email: ['iva@gmail.com'],
+    password: ['']
+  })
+  constructor(private formBuilder: FormBuilder){
 
   }
   ngOnInit(): void {
     console.log('entre a mi login')
   }
 }
+
+
+
 
